5b5564f8e9d9cc83d12fbcbce6f0d504b27aa7f6
hendrakrn23 <hendrakurniawan2304@gmail.com>
2018-08-10 16:18:02 +0700

    Push revisi component All003 ALL004 PJA005 PJA006

--- src/app/web/pages/PJA/PJA006/PJA006.component.ts
+++ src/app/web/pages/PJA/PJA006/PJA006.component.ts
@@ -2,10 +2,8 @@ import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { InputForm } from '../../../../core/models/input-form';
 import { ActionService } from '../../../../core/services/uninjectable/action.service';
 import { CoreFactory } from '../../../../core/factory/core.factory';
-import { DataTable } from '../../../../core/models/data-table';
-import { TYPE, CONJUNCTION_OPERATOR, COMPARISON_OPERATOR } from '../../../../core/constant/constant';
+import { COMPARISON_OPERATOR } from '../../../../core/constant/constant';
 import { ActivatedRoute, ParamMap } from '../../../../../../node_modules/@angular/router';
-import { Observable } from '../../../../../../node_modules/rxjs/Observable';
 
 @Component({
   selector: 'app-PJA006',
@@ -16,7 +14,6 @@ export class PJA006Component implements OnInit {
 public action: ActionService;
 public inputForm: InputForm;
 public time: Date = new Date();
-public dataTable: DataTable;
 
 @ViewChild('viewAsDateTemplate')
 public viewAsDateTemplate: any;
@@ -83,59 +80,17 @@ private selectedId: string;
       console.log(res);
       this.action.patchFormData(res.data.items[0]);
     });
-
-  //   this.dataTable = this._factory.dataTable({
-  //   pagingParams: {
-  //     limit: 10
-  //   },
-  //   serverSide: true,
-  //   searchCriteria: [
-  //     { viewValue: 'Client ID', viewKey: 'client_id', type: TYPE.NUMBER}
-  //   ],
-  //   tableColumns: [
-  //     { prop: 'client_id', name: 'Client Id', width: 100 },
-  //     {
-  //       prop: 'id', name: 'Action', width: 100,
-  //       cellTemplate: this.tableActionTemplate, sortable: false
-  //     }
-  //   ]
-  // });
-
-  // first Action Init
-  //   this.action = this._factory.actions({
-  //   api: 'project/MengelolaClient/',
-  //   inputForm: this.inputForm,
-  //   dataTable: this.dataTable
-  // });
-
-  //   let readAllApi = this._factory.api({
-  //   api: 'project/MengelolaClient/ReadAll',
-  //   pagingParams: {
-  //     filter: {
-  //       operator: CONJUNCTION_OPERATOR.AND,
-  //       component: [{
-  //         field: 'client_id',
-  //         operator: COMPARISON_OPERATOR.EQ,
-  //         value: this.dataClient
-  //       }]
-  //     }
-  //   },
-  //   params: {
-  //     client_id: this.dataClient
-  //   }
-  // });
-  //   this._factory.http()
-  // .get(readAllApi)
-  // .subscribe((response: any) => {
-  //   if (JSON.parse(response._body).data) {
-  //     this.data = JSON.parse(response._body).data[0];
-  //   }
-  // });
   }
 
   public onEdit() {
+    const updateAPI = this._factory.api({
+      api : 'project/MengelolaClient/update',
+      // params : {
+      //   client_id : this.selectedId
+      // }
+    });
 
-    this._factory.http().put('localhost:7979/project/MengelolaClient/update?client_id=' + this.selectedId, this.action.getFormData()).subscribe((response: any) => {
+    this._factory.http().put(updateAPI + '?client_id=' + this.selectedId, this.action.getFormData()).subscribe((response: any) => {
       console.log('Berhasil');
     });
 
