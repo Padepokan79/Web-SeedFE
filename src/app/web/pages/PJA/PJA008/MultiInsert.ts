1048b9bb348c38b6a550e60da4e019bd1275db42
khairil89 <khairil.anwar852@gmail.com>
2018-08-21 14:18:12 +0700

    Update PJA012, PJA008, ALL006

--- src/app/web/pages/PJA/PJA008/MultiInsert.ts
+++ src/app/web/pages/PJA/PJA008/MultiInsert.ts
@@ -1,30 +1,5 @@
-import { HttpParams, HttpClient } from '@angular/common/http';
-
 export class MultiInsert {
-    public clientId: number;
+    public clientId: number = 4;
     public sdmId: string = '';
     public hirestatId: number = 3;
-
-    public apiRoot: string = 'api/project/MengelolaHiring';
-
-    constructor(private httpClient: HttpClient) {
-
-    }
-
-    public postSdmHiring() {
-        const urlPost = '${this.apiRoot}/post';
-        const httpOptions = {
-            params : new HttpParams()
-        };
-        this.httpClient.post(urlPost, {
-            listData: [
-                {
-                    client_id: this.clientId,
-                    sdm_id: this.sdmId,
-                    hirestat_id: this.hirestatId
-                },
-            ]
-        }, httpOptions)
-        .subscribe((res) => console.log(res));
-    }
 }
--- src/app/web/pages/PJA/PJA012/MultiInsertSdmAssign.ts
+++ src/app/web/pages/PJA/PJA012/MultiInsertSdmAssign.ts
@@ -1,35 +1,9 @@
-import { HttpParams, HttpClient } from '@angular/common/http';
-
 export class MultiInsertSdmAssign {
-    public methodId: string = '';
+    public methodId: string = 'ONSITE';
     public sdmhiringId: string = '';
-    public sdmassignStartdate: string = '';
-    public sdmassignEnddate: string = '';
-    public sdmassignLoc: string = '';
-    public sdmassignPicclient: string = '';
-    public sdmassignPicclientphone: string = '';
-    public apiRoot: string = 'api/project/SdmAssignment';
-
-    constructor(private http: HttpClient) {
-
-    }
-
-    public postSdmAssignment() {
-        const urlPost = '${this.apiRoot}/post';
-        const httpOptions = {
-            params : new HttpParams()
-        };
-        this.http.post(urlPost, {
-            listData: [{
-                method_id: this.methodId,
-                sdmhiring_id: this.sdmhiringId,
-                sdmassign_startdate: this.sdmassignStartdate,
-                sdmassign_enddate: this.sdmassignEnddate,
-                sdmassign_loc: this.sdmassignLoc,
-                sdmassign_picclient: this.sdmassignPicclient,
-                sdmassign_picclientphone: this.sdmassignPicclientphone
-            }]
-        }, httpOptions)
-        .subscribe((res) => console.log(res));
-    }
+    public sdmassignStartdate: string = '2018-01-01';
+    public sdmassignEnddate: string = '2019-01-01';
+    public sdmassignLoc: string = 'Bandung';
+    public sdmassignPicclient: string = 'Dia';
+    public sdmassignPicclientphone: string = '080989999';
 }
