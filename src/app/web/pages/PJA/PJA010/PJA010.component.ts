e4baa4883646d2934e9b511bc8cea3a5f3fb12a3
jodyms <jodydiva@gmail.com>
2018-10-16 12:38:59 +0700

    Revisi 16-10-2018

--- src/app/web/pages/PJA/PJA010/PJA010.component.ts
+++ src/app/web/pages/PJA/PJA010/PJA010.component.ts
@@ -153,15 +153,48 @@ export class PJA010Component implements OnInit {
   }
 
   public onSearch() {
+    this.ambilData();
+    this.dataTable = null ;
     const ClientId = this.action.getFormControlValue('client_id');
 
-    this.action.setPaginationFilter(
-      Conjunction.OR(
-        Comparison.EQ('client_id', ClientId),
-      )
-    );
-
+    // this.action.setPaginationFilter(
+    //   // Conjunction.OR(
+    //     Comparison.EQ('client_id', ClientId),
+    //   // )
+    // );
+    this.dataTable = this._factory.dataTable({
+      serverSide : false,
+      pagingParams : {
+        limit : 10,
+        filter : Comparison.EQ('client_id', ClientId)
+      },
+      searchCriteria : [
+        { viewValue: 'Name', viewKey: 'sdm_name', type: TYPE.STRING },
+        { viewValue: 'PIC Handler', viewKey: 'sdmassign_picclient', type: TYPE.STRING }
+      ],
+      tableColumns : [
+        { prop: 'norut', name: 'No', flexGrow: 1, sortable: false },
+        { prop: 'sdm_name', name: 'Name', flexGrow: 3, sortable: false },
+        { prop: 'sdm_phone', name: 'Contact', flexGrow: 2, sortable: false },
+        { prop: 'sdmassign_startdate', name: 'Start', flexGrow: 2, sortable: false },
+        { prop: 'sdmassign_enddate', name: 'End', flexGrow: 2, sortable: false },
+        { prop: 'sdmassign_loc', name: 'Location', flexGrow: 2, sortable: false },
+        { prop: 'sdmassign_picclient', name: 'PIC Handler', flexGrow: 2, sortable: false },
+        { prop: 'sdmassign_picclientphone', name: 'PIC Contact', flexGrow: 3, sortable: false },
+        { prop: 'method_name', name: 'Method', flexGrow: 2, sortable: false },
+        { prop: 'sdmassign_notification', name: 'Notification Assignment', flexGrow: 3,
+          cellTemplate: this.notif, sortable: false },
+        { prop: 'sdmassign_id', name: 'Action', flexGrow: 2,
+          cellTemplate: this.tableActionTemplate, sortable: false }
+      ]
+    });
+    this.action = this._factory.actions({
+      api: 'project/SdmAssignment',
+      inputForm: this.inputForm,
+      dataTable: this.dataTable
+  });
     this.action.refreshTable();
+    this.action.patchFormData({client_id : ClientId})
   }
 
   public navigateEditMenu(id) {
@@ -202,5 +235,4 @@ public onDelete(id, deleteMassage: string = 'Are you sure to delete?') {
         this.action.refreshTable();
       });
 }
-
 }
